5e4b1ff69b5947f4228e39ec9086bd08108bbadb
Jaobast <153824183+Jaobast@users.noreply.github.com>
2024-08-22 13:18:59 +0200

    init

---
+++ script/array.js
@@ -0,0 +1,92 @@
+const productData = [ 
+    {
+        name: 'Burger Smash',
+        description: '160g saftiges Rindfleisch, perfekt „gesmasht“ für eine knusprige Kruste, dazu geschmolzener Cheddar, frischer Salat, Tomaten, rote Zwiebeln und unsere würzige Haus-Sauce im getoasteten Brioche-Brötchen. Der ultimative Genuss!',
+        pics:'./assets/burgers/hamb-1.png',
+        price: '12.90',
+        kcal: '330'
+    },
+
+
+    {
+        name: 'Burger Double',
+        description: 'Doppelt saftiges Rindfleisch (2x160g), geschmolzener Cheddar, frischer Salat, Tomaten, Zwiebeln und BBQ-Sauce im getoasteten Brioche-Brötchen. Perfekt für großen Hunger und vollen Genuss.',
+        pics:'./assets/burgers/hamb-2.png',
+        price: '15.90',
+        kcal: '380'
+    },
+
+
+    {
+        name: 'Burger Salad',
+        description: 'Ein frischer Mix aus knackigem Salat, saftigen Tomaten, Gurken und roten Zwiebeln, verfeinert mit herzhaften Burger-Streifen und einem leichten Dressing. Ideal als leichte, aber dennoch geschmackvolle Mahlzeit.',
+        pics:'./assets/burgers/hamb-8.png',
+        price: '12.50',
+        kcal: '250'
+    },
+
+
+    {
+        name: 'Burger Eggs',
+        description: 'Herzhafte Rinderhackfleischbällchen kombiniert mit perfekt gebratenen Eiern – wahlweise als Spiegelei oder Rührei. Serviert mit frischem Gemüse oder knusprigem Brot, für ein sättigendes und leckeres Gericht.',
+        pics:'./assets/burgers/hamb-3.png',
+        price: '13.90',
+        kcal: '330'
+    },
+
+
+    {
+        name: 'Burger Rings',
+        description: 'Knusprige Zwiebelringe, goldbraun frittiert und perfekt gewürzt. Diese leckeren Beilagen ergänzen jeden Burger ideal und bieten einen unwiderstehlich knusprigen Crunch mit jedem Bissen.',
+        pics:'./assets/burgers/hamb-7.png',
+        price: '15.90',
+        kcal: '350'
+    },
+
+
+    {
+        name: 'Tech Burger',
+        description: 'Unsere Spezialität des Hauses. Saftiges Rindfleisch-Patty mit Cheddar, Speck, Salat, Tomaten und Zwiebeln, serviert in einem getoasteten Brioche-Brötchen mit hausgemachter Sauce. Der perfekte Burger!',
+        pics:'./assets/burgers/hamb-6.png',
+        price: '15.90',
+        kcal: '380'
+    },
+
+
+    {
+        name: 'Wasser 1 L',
+        description: '1 Liter Wasser. Medium. Vöslauer',
+        pics:'./assets/drinks/refri-2.png',
+        price: '2.50',
+        kcal: '0'
+    },
+
+
+    {
+        name: 'Cola 350 ml',
+        description: '350 ml Coca Cola. Classic',
+        pics:'./assets/drinks/refri-1.png',
+        price: '3.50',
+        kcal: '157'
+    },
+    
+
+    {
+        name: 'Cola Zero 350 ml',
+        description: '350 ml Coca Cola. Zero Kcal',
+        pics:'./assets/drinks/refri-3.png',
+        price: '3.50',
+        kcal: '0'
+    },
+
+
+    {
+        name: 'Fanta 350 ml',
+        description: '350 ml Fanta Orange',
+        pics:'./assets/drinks/refri-4.png',
+        price: '3.50',
+        kcal: '157'
+    },
+]
+
+
---
+++ script/script.js
@@ -0,0 +1,253 @@
+const menu = document.getElementById("menu");
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("modal");
+const cartItems = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const closeBtn = document.getElementById("close-modal-btn");
+const checkoutBtn = document.getElementById("checkout-modal-btn");
+const cartCounter = document.getElementById("cart-count");
+const adressInput = document.getElementById("adresse");
+const adressWarn = document.getElementById("adresse-warn");
+
+const productModal = document.getElementById("modal-product");
+const products = document.querySelectorAll(".name-product");
+
+let cart = [];
+
+
+// Abrir o modal
+cartBtn.addEventListener('click', function(){
+    cartModal.classList.remove("hidden");
+    document.body.style.position = "fixed";
+    updateCartModal();
+})
+
+// Fechar o modal
+closeBtn.addEventListener('click', function(){
+    cartModal.classList.add("hidden");
+    document.body.style.position = "";
+})
+
+cartModal.addEventListener('click',function(e){
+    if(e.target === cartModal){
+        cartModal.classList.add("hidden");
+        document.body.style.position = "";
+    }
+})
+
+// Pegar valores dos produtos
+
+menu.addEventListener('click', function(e){
+    let parentButton = e.target.closest(".button_buy");
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name");
+        const price = parseFloat(parentButton.getAttribute("data-price"));
+
+        addToCart(name, price);
+    }
+
+})
+
+productModal.addEventListener('click', function(e){
+    let parentButton = e.target.closest(".button_buy");
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name");
+        const price = parseFloat(parentButton.getAttribute("data-price"));
+
+        addToCart(name, price);
+    }
+
+})
+
+// Adidionar no carrinho
+
+function addToCart(name, price){
+    const existingItem = cart.find(item => item.name === name)
+    if(existingItem){ existingItem.quantity += 1;}
+    else {
+        cart.push({
+            name,
+            price,
+            quantity: 1
+        })
+    }
+
+    updateCartModal();
+}
+
+// Atualiza carrinho
+
+function updateCartModal(){
+    cartItems.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+
+        cartItemElement.innerHTML =`
+        <div class="container-item">
+           <div>
+              <p class="name">${item.name}</p>
+              <p>Anzahl: ${item.quantity}</p>
+              <p>Preis: ${item.price.toFixed(2)}€</p>
+           </div>
+
+           <button class="remove" data-name="${item.name}">Entfernen</button>
+
+        </div>
+        `
+
+        total += item.price * item.quantity;
+
+        cartItems.appendChild(cartItemElement);
+    })
+
+// Total da compra
+    cartTotal.textContent = total.toLocaleString("de-DE", {
+        style: "currency",
+        currency: "EUR"
+    });
+
+//quantos intens foram adicionados no carrinho
+    cartCounter.innerHTML = cart.length;
+}
+
+//Remover item 
+cartItems.addEventListener('click', function(e){
+    if(e.target.classList.contains("remove")){
+        const name = e.target.getAttribute("data-name")
+
+        removeItem(name);
+    }
+})
+
+function removeItem(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+        const item = cart[index];
+
+        if(item.quantity > 1){
+            item.quantity -= 1;
+            updateCartModal();
+            return;
+        }
+
+        cart.splice(index,1);
+        updateCartModal();
+    }
+}
+
+//Endereço
+adressInput.addEventListener('input', function(e){
+    let inputValue = e.target.value;
+
+    if(inputValue !== ""){
+        adressWarn.classList.add("hidden");
+        adressInput.classList.remove("btn-warn");
+    }
+})
+
+checkoutBtn.addEventListener('click', function(){
+    const isOpen = checkRestaurantOpen();
+
+    if(cart.length === 0)return;
+    if(adressInput.value === ""){
+        adressWarn.classList.remove("hidden");
+        adressInput.classList.add("btn-warn");
+        return
+    }
+
+    if(!isOpen){
+        Toastify({
+            text: "Ops. Das Restaurant ist geschlossen!",
+            duration: 5000,
+            close: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+              background: "var(--red)", borderRadius: "15px",
+              boxShadow: "0 0 0 rgba(0, 0, 0, 0)",
+            },
+        }).showToast();
+    }
+
+    if(isOpen){
+        Toastify({
+            text: "Deine Bestellung wurde abgeschlossen!",
+            duration: 5000,
+            close: true,
+            gravity: "top",
+            position: "right",
+            stopOnFocus: true,
+            style: {
+              background: "var(--green)", borderRadius: "15px",
+              boxShadow: "0 0 0 rgba(0, 0, 0, 0)",
+            },
+        }).showToast();
+    }
+
+
+    cart = [];
+    adressInput.value = "";
+    updateCartModal();
+})
+
+
+//Checar se esta aberto
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 10 && hora < 22;
+}
+
+const spanItem = document.getElementById("date-span");
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.style.backgroundColor = "var(--green)"
+}else{ spanItem.style.backgroundColor = "var(--red)"}
+
+
+
+
+
+
+
+
+
+
+products.forEach(function(product, index) {
+    product.addEventListener('click', function() {
+
+        const modalImg = productModal.querySelector('.cover img');
+        const modalTitle = productModal.querySelector('.titel h3');
+        const modalKcal = productModal.querySelector('.kcal');
+        const modalDescription = productModal.querySelector('.description');
+        const modalPrice = productModal.querySelector('.price');
+
+        
+        modalImg.src = productData[index].pics;
+        modalTitle.textContent = productData[index].name;
+        modalKcal.textContent = productData[index].kcal + ' kcal';
+        modalDescription.textContent = productData[index].description;
+        modalPrice.textContent = productData[index].price + '€';
+
+        const modalButtonBuy = productModal.querySelector('.button_buy');
+        modalButtonBuy.setAttribute('data-name', productData[index].name);
+        modalButtonBuy.setAttribute('data-price', productData[index].price);
+
+
+        productModal.classList.remove("hidden");
+    });
+});
+
+
+productModal.addEventListener('click', function(){
+    productModal.classList.add("hidden");
+    document.body.style.position = "";
+})
+
+
+
