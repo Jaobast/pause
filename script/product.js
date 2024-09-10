d3e5591352d50087a16ac78faa765e668f76c4d9
Jaobast <153824183+Jaobast@users.noreply.github.com>
2024-09-10 22:16:22 +0200

    product08

--- script/product.js
+++ script/product.js
@@ -21,6 +21,7 @@ products.forEach(function (product) {
 
     if (selectedProduct) {
         product.addEventListener('click', function () {
+            window.scrollTo(0, 0);
 
 
             const detailImg = productDetail.querySelector('img');
@@ -36,17 +37,24 @@ products.forEach(function (product) {
             detailDescription.textContent = selectedProduct.description;
 
 
-            window.scrollTo(0, 0);
+
             productDetail.classList.remove("hidden");
 
 
             if (product.classList.contains('burger-veggie') || product.classList.contains('product-popular-veggie')) {
                 productDetail.style.backgroundColor = 'var(--green)';
                 detailName.style.color = 'var(--green)';
+
+                document.getElementById('theme-color-meta').setAttribute('content', '#045204');
                 if (buttonVeggie) {
                     buttonVeggie.style.backgroundColor = 'var(--green)';
                 }
-            }
+            } else {
+                productDetail.style.backgroundColor = 'var(--red)';
+                detailName.style.color = 'var(--red)';
+                buttonVeggie.style.backgroundColor = 'var(--red)';
+                document.getElementById('theme-color-meta').setAttribute('content', '#900D0D');
+            } 
             
         });
     } else {
@@ -56,6 +64,8 @@ products.forEach(function (product) {
 
 detailClose.addEventListener('click', function () {
     window.scrollTo(0, 0);
+
+    document.getElementById('theme-color-meta').setAttribute('content', '#f4e6ce');
 
     productDetail.classList.add("hidden");
 
